600142f20afc98b94171c5ad4b982e95414f00fd
Brian Bolli <brian.bolli@arctg.com>
2017-08-03 10:47:47 -0500

    Release merge

--- bin/azuresync.js
+++ bin/azuresync.js
@@ -1,7 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#!/usr/bin/env node
+
+var azuresync = require('../index');
+
+console.log('I worked?');
 
 
